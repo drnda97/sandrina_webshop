3b6d1dabdb72662c5e1697b5fb75d4efbd10b6fe
Milan <drndica54@gmil.com>
2020-01-14 14:31:16 +0100

    app name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/main.css')}}">
-        <title>Webshop</title>
+        <title>{{config('app.name')}}</title>
     </head>
     <body>
         <div class="container">
