e18044705f44181c8319d869140d3281d5781e60
Milan <drndica54@gmil.com>
2020-01-14 14:01:48 +0100

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/main.css')}}">
+        <title>Webshop</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                @include('layouts/nav')
+                @yield('content')
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,11 @@
+<nav>
+    <div class="wrapper">
+        <div class="logo"><a href="/"><img src="{{asset('images/logo/logo.png')}}" alt="logo"></a></div>
+        <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Gallery</a></li>
+            <li><a href="#">Collection</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>    
+</nav>
