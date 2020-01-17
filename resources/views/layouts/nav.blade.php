0e533544ea0e43c4b0df715218b9b7794f27a58d
Milan <drndica54@gmil.com>
2020-01-17 11:46:41 +0100

    working on navigation

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,11 +1,19 @@
 <nav>
-    <div class="wrapper">
-        <div class="logo"><a href="/"><img src="{{asset('images/logo/logo.png')}}" alt="logo"></a></div>
+    <div class="logo">
+        <a href="/"><img src="{{asset('images/logo/logo.png')}}" alt="logo"></a>
+        <div class="user-pages">
+            <ul>
+                <li><a href="#"><img src="" alt="sign-in-icon">Sign in</a></li>
+                <li><a href="#"><img src="" alt="sign-in-icon">View Cart</a></li>
+            </ul>
+        </div>  
+    </div>
+    <div class="pages">
         <ul>
             <li><a href="/about">About</a></li>
             <li><a href="/gallery">Gallery</a></li>
             <li><a href="/collection">Collection</a></li>
             <li><a href="/contact">Contact</a></li>
         </ul>
-    </div>    
+    </div>  
 </nav>
